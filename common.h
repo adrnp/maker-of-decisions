7948a6f136ccdc17b893d287f20ed65a3c0d931f
Adrien Perkins <adrienp@stanford.edu>
2015-01-29 12:14:58 -0800

    adding file for common variables, currently just debug and verbose variables

---
+++ common.h
@@ -0,0 +1,21 @@
+/*
+ * common.h
+ *
+ *  Created on: Jan 23, 2015
+ *      Author: adrienp
+ */
+
+#ifndef COMMON_H_
+#define COMMON_H_
+
+
+/** boolean to determine whether or not to show all outputs */
+extern bool verbose;
+
+/** boolean to determine whether or not to show debug outputs */
+extern bool debug;
+
+
+
+
+#endif /* COMMON_H_ */
