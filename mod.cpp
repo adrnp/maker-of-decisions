7a1bec98e75b14ad16b8a5c45a07e95f54c47121
Adrien Perkins <adrienp@stanford.edu>
2015-01-29 12:59:25 -0800

    clean up the main script file to be more streamlined readable

--- mod.cpp
+++ mod.cpp
@@ -6,11 +6,76 @@
  */
 
 #include "mod.h"
-#include "port_setup.h"
-#include "read_thread.h"
+#include "serial_port.h"
+
+#include "common.h"
 
 using std::string;
 using namespace std;
+
+
+// variable declarations
+
+bool verbose = false;  // default verbose to false
+bool debug = false;    // default debug to false
+
+
+/**
+ * read in the passed arguments to the function on start
+ *
+ * TODO: figure out what to return and how to handle uart and baud values
+ */
+void read_arguments(int argc, char **argv, char **uart_name, int *baudrate) {
+
+	// string to be displayed on incorrect inputs to show correct function usage
+	const char *commandline_usage = "\tusage: %s -d <devicename> -b <baudrate> [options]\n\n"
+			"\t-v/--verbose\t\t detailed output of current state\n"
+			"\n\t\tdefault: -d %s -b %i\n";
+
+	// loop through all the program arguments
+	for (int i = 1; i < argc; i++) { /* argv[0] is "mavlink" */
+
+		// help text requested
+		if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0) {
+			printf(commandline_usage, argv[0], *uart_name, *baudrate);
+			throw EXIT_FAILURE;
+		}
+
+		/* UART device ID */
+		if (strcmp(argv[i], "-d") == 0 || strcmp(argv[i], "--device") == 0) {
+			if (argc > i + 1) {
+				*uart_name = argv[i + 1];
+
+			} else {
+				printf(commandline_usage, argv[0], *uart_name, *baudrate);
+				throw EXIT_FAILURE;
+			}
+		}
+
+		/* baud rate */
+		if (strcmp(argv[i], "-b") == 0 || strcmp(argv[i], "--baud") == 0) {
+			if (argc > i + 1) {
+				*baudrate = atoi(argv[i + 1]);
+
+			} else {
+				printf(commandline_usage, argv[0], *uart_name, *baudrate);
+				throw EXIT_FAILURE;
+			}
+		}
+
+		/* verbosity */
+		if (strcmp(argv[i], "-v") == 0 || strcmp(argv[i], "--verbose") == 0) {
+			verbose = true;
+		}
+
+		/* debug */
+		if (strcmp(argv[i], "-v") == 0 || strcmp(argv[i], "--debug") == 0) {
+			debug = true;
+		}
+
+	}
+}
+
 
 
 int main(int argc, char **argv) {
@@ -21,108 +86,23 @@ int main(int argc, char **argv) {
 	/* default values for arguments */
 	char *uart_name = (char*)"/dev/ttyUSB0";
 	int baudrate = 115200;
-	const char *commandline_usage = "\tusage: %s -d <devicename> -b <baudrate> [options]\n\n"
-			"\t-v/--verbose\t\t detailed output of current state\n"
-			"\n\t\tdefault: -d %s -b %i\n";
 
-	/* read program arguments */
-	int i;
+	// read the input arguments
+	read_arguments(argc, argv, &uart_name, &baudrate);
 
-	for (i = 1; i < argc; i++) { /* argv[0] is "mavlink" */
-		if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0) {
-			printf(commandline_usage, argv[0], uart_name, baudrate);
-			return 0;
-		}
-
-		/* UART device ID */
-		if (strcmp(argv[i], "-d") == 0 || strcmp(argv[i], "--device") == 0) {
-			if (argc > i + 1) {
-				uart_name = argv[i + 1];
-
-			} else {
-				printf(commandline_usage, argv[0], uart_name, baudrate);
-				return 0;
-			}
-		}
-
-		/* baud rate */
-		if (strcmp(argv[i], "-b") == 0 || strcmp(argv[i], "--baud") == 0) {
-			if (argc > i + 1) {
-				baudrate = atoi(argv[i + 1]);
-
-			} else {
-				printf(commandline_usage, argv[0], uart_name, baudrate);
-				return 0;
-			}
-		}
-
-		/* verbosity */
-		if (strcmp(argv[i], "-v") == 0 || strcmp(argv[i], "--verbose") == 0) {
-			// TODO: add levels of verbosity
-			silent = false;
-			verbose = true;
-			printAll = true;
-		}
-
-	}
-
-	// SETUP SERIAL PORT
-
-	// Exit if opening port failed
-	// Open the serial port.
-	if (!silent) printf("Trying to connect to %s.. ", uart_name);
-	fflush(stdout);
-
-	fd = open_port(uart_name);
-	if (fd == -1)
-	{
-		if (!silent) printf("failure, could not open port.\n");
-		exit(EXIT_FAILURE);
-	}
-	else
-	{
-		if (!silent) printf("success.\n");
-	}
-	if (!silent) printf("Trying to configure %s.. ", uart_name);
-	bool setup = setup_port(fd, baudrate, 8, 1, false, false);
-	if (!setup)
-	{
-		if (!silent) printf("failure, could not configure port.\n");
-		exit(EXIT_FAILURE);
-	}
-	else
-	{
-		if (!silent) printf("success.\n");
-	}
-
-	int noErrors = 0;
-	if (fd == -1 || fd == 0)
-	{
-		if (!silent) fprintf(stderr, "Connection attempt to port %s with %d baud, 8N1 failed, exiting.\n", uart_name, baudrate);
-		exit(EXIT_FAILURE);
-	}
-	else
-	{
-		if (!silent) fprintf(stderr, "\nConnected to %s with %d baud, 8 data bits, no parity, 1 stop bit (8N1)\n", uart_name, baudrate);
-	}
-
-	if(fd < 0)
-	{
-		exit(noErrors);
-	}
-
-	// Run indefinitely while the serial loop handles data
-	if (!silent) printf("\nREADY, waiting for heartbeat.\n");
+	// open and configure the com port being used for communication
+	begin_serial(uart_name, baudrate);
 
 
 	// need to create read and write threads
+	/*
 	pthread_create(&readId, NULL, serial_read, (void *)&uav);
 
 
 	pthread_join(readId, NULL);
+	*/
 
-
-	close_port(fd);
+	end_serial();
 
 	return 0;
 }
--- mod.h
+++ mod.h
@@ -9,16 +9,32 @@
 #define MOD_H_
 
 // all the standard includes
-#include "common_include.h"
+// include "common_include.h"
 
 
 // include common variables
-#include "common_vars.h"
+// #include "common_vars.h"
 
+// system inclusions
+#include <iostream>
+#include <cstdlib>
+#include <unistd.h>
+#include <cmath>
+#include <string.h>
+#include <inttypes.h>
+#include <fstream>
+#include <signal.h>
+
+
+/* read in the command line arguments */
+void read_arguments(int argc, char **argv, char **uart_name, char *baudrate);
+
+/* main function */
+int main(int argc, char **argv);
 
 
 // just putting this here as a placeholder for now
-void generateCommand();
+// void generateCommand();
 
 
 #endif /* MOD_H_ */
