d11223d2ccb8683afd9964bf982a6abca109d1d8
Adrien Perkins <adrienp@stanford.edu>
2015-07-23 11:49:30 -0700

    add bash script for starting mod to avoid having to retype all the input commands every time

---
+++ mod_startup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# define the necessary variables
+
+# pixhawk information
+PIXHAWK_PORT=/dev/ttyUSB1
+PIXHAWK_BAUD=115200
+
+# wifly information
+WIFLY1_PORT=/dev/ttyUSB0
+
+WIFLY2_ENABLED=0
+WIFLY2_PORT=/dev/ttyUSB2
+
+if [ $WIFLY2_ENABLED -eq 0 ]; then
+	./mod -d $PIXHAWK_PORT -b $PIXHAWK_BAUD -w1 $WIFLY1_PORT -c -v
+else
+	./mod -d $PIXHAWK_PORT -b $PIXHAWK_BAUD -w1 $WIFLY1_PORT -w2 $WIFLY2_PORT -c -v
+fi
