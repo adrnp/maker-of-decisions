16c856dbd4636753a5e8bdae18878a9e6bc16b72
Adrien Perkins <adrienp@stanford.edu>
2015-08-04 15:54:35 -0700

    change for output to dump to log file

--- mod_startup.sh
+++ mod_startup.sh
@@ -3,17 +3,17 @@
 # define the necessary variables
 
 # pixhawk information
-PIXHAWK_PORT=/dev/ttyUSB0
+PIXHAWK_PORT=/dev/ttyUSB1
 PIXHAWK_BAUD=115200
 
 # wifly information
-WIFLY1_PORT=/dev/ttyUSB1
+WIFLY1_PORT=/dev/ttyUSB0
 
 WIFLY2_ENABLED=0
 WIFLY2_PORT=/dev/ttyUSB2
 
 if [ $WIFLY2_ENABLED -eq 0 ]; then
-	./mod -d $PIXHAWK_PORT -b $PIXHAWK_BAUD -w1 $WIFLY1_PORT -c -v
+	./mod -d $PIXHAWK_PORT -b $PIXHAWK_BAUD -w1 $WIFLY1_PORT -c -v  >> logFile.log
 else
 	./mod -d $PIXHAWK_PORT -b $PIXHAWK_BAUD -w1 $WIFLY1_PORT -w2 $WIFLY2_PORT -c -v
 fi
