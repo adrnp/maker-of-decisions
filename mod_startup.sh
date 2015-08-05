cd0212a3ac9bbb90b863ed318b14d48c5abc9fed
Adrien Perkins <adrienp@stanford.edu>
2015-08-05 15:35:33 -0700

    add tracking command flag

--- mod_startup.sh
+++ mod_startup.sh
@@ -13,7 +13,7 @@ WIFLY2_ENABLED=0
 WIFLY2_PORT=/dev/ttyUSB2
 
 if [ $WIFLY2_ENABLED -eq 0 ]; then
-	./mod -d $PIXHAWK_PORT -b $PIXHAWK_BAUD -w1 $WIFLY1_PORT -c -v #  >> logFile.log
+	./mod -d $PIXHAWK_PORT -b $PIXHAWK_BAUD -w1 $WIFLY1_PORT -c -v -t# >> logFile.log
 else
 	./mod -d $PIXHAWK_PORT -b $PIXHAWK_BAUD -w1 $WIFLY1_PORT -w2 $WIFLY2_PORT -c -v
 fi
